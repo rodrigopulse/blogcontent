a01119b4b55957290ff19898148b87ab2e2adce3
rodrigopulse <contato@rodrigogodoy.net>
2019-04-05 14:08:18 -0300

    atualizado para gulp 4

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,38 +11,33 @@ var uglify 		= require('gulp-uglify');
 var prefixerOptions = {
 	browsers: ['last 3 versions']
 };
-gulp.task( 'sass', function() {
-	gulp.src( 'src/assets/sass/style.scss' )
+gulp.task( 'sass', function(done) {
+	return gulp.src( 'assets/sass/style.scss' )
 	.pipe( sourcemaps.init())
 	.pipe( sass({outputStyle: 'compressed'}).on('error', sass.logError) )
 	.pipe( sourcemaps.write( './assets/maps' ) )
-    .pipe( gulp.dest( './' ) );
+	.pipe( gulp.dest( './' ) );
+	done();
 } );
 
 //JavaScript
 var jsFiles = [
-	'src/assets/js/scripts.js'
+	'assets/js/scripts.js'
 ];
-gulp.task( 'javascript', function() {
-	gulp.src( jsFiles )
+gulp.task( 'javascript', function(done) {
+	return gulp.src( jsFiles )
     .pipe(concat('scripts.js'))
 	.pipe(uglify({mangle:false}))
-    .pipe(gulp.dest('./'));
+	.pipe(gulp.dest('./'));
+	done();
 } );
 
 // Default
-gulp.task( 'default', [ 'sass', 'javascript' ] );
+gulp.task( 'default', gulp.series('sass', 'javascript') );
 
 // Watch
 gulp.task('watch', function() {
-    gulp.watch( 'src/assets/sass/**/*.scss', ['sass'] );
-    gulp.watch( 'src/assets/sass/style.scss', ['sass'] );
-    gulp.watch( jsFiles, ['javascript'] );
+	gulp.watch( 'assets/sass/**/*.scss', gulp.parallel( ['sass'] ));
+    gulp.watch( 'assets/sass/style.scss', gulp.parallel( ['sass'] ));
+    gulp.watch( jsFiles, gulp.parallel( ['javascript'] ));
 } );
-
-gulp.task('browser-sync', function() {
-    browserSync.init('', {
-		proxy: "http://localhost",
-		notify: false
-    });
-});
